3a5ff0f1c4d89fc8cf8ce9f6df53331b69b998d2
Sam Dorius <doriussam@gmail.com>
2023-09-05 15:51:44 -0600

    finished sign up

--- src/Backend/db.js
+++ src/Backend/db.js
@@ -1,21 +1,21 @@
 import { Sequelize } from 'sequelize';
 
 async function connectToDB(dbURI) {
+  console.log(`Connecting to DB: ${dbURI}`);
 
   const sequelize = new Sequelize(dbURI, {
-    logging: false,
+    logging: console.log, // set logging: false to disable outputting SQL queries to console
     define: {
-      timestamps: false,
-      underscored: true,
+      timestamps: false, // don't want created_at or updated_at columns
+      underscored: true, // use snake_case rather than camelCase column names
     },
-    dialect: 'postgresql',
-  })
+  });
 
   try {
     await sequelize.authenticate();
-    console.log("Connected to DB successfully");
+    console.log('Connected to DB successfully!');
   } catch (error) {
-    console.error(`Unable to connect to DB: ${dbURI}`, error);
+    console.error('Unable to connect to DB:', error);
   }
 
   return sequelize;
---
+++ src/Frontend/SignUp.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react"
+import axios from "axios"
+import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+import { first } from "lodash"
+
+
+export default function SignUp()
+{
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [errMsg, setErrMsg] = useState('')
+    const [err, setErr] = useState('')
+
+    const navigate = useNavigate()
+
+    const ClickSignUp = async () =>
+    {
+        const user = {email: email, password: password, firstName: firstName, lastName: lastName}
+
+        if (email !== '' && password !== '' && firstName !== '' && lastName !== '')
+        {
+            const {data} = await axios.post('/api/signup', user)
+    
+            if (data.message === 'account created')
+            {
+                navigate('/login')
+            }
+            else if (data.message === 'user already exists')
+            {
+                setErr('error')
+                setErrMsg(data.message)
+            }
+        }
+        else
+        {
+            setErr('error')
+            setErrMsg('please fill out every text box')
+        }
+    }
+
+    return (
+        <div>
+            <div>
+                <input type="text" placeholder="email" onChange={(event) => {setEmail(event.target.value)}} />
+                <input type="text" placeholder="password" onChange={(event) => {setPassword(event.target.value)}} />
+            </div>
+            <div>
+                <input type="text" placeholder="First Name" onChange={(event) => {setFirstName(event.target.value)}} />
+                <input type="text" placeholder="Last Name" onChange={(event) => {setLastName(event.target.value)}} />
+            </div>
+            <input type="submit" onClick={ClickSignUp}/>
+            { err.length > 0 &&
+                <div>{errMsg}</div>
+            }
+        </div>
+    )
+}
