fddc0c055a49a8357084da09c969017fa33107f8
Tyler Jerman <Tyler.s.jerman@gmail.com>
2023-09-11 16:01:04 -0600

    add and delete comment done

--- src/Frontend/Comments.jsx
+++ src/Frontend/Comments.jsx
@@ -1,18 +1,56 @@
 import React from 'react';
-import AddComment from './commentsFolder/AddComment.jsx'
+import AddComment from './commentsFolder/addComment';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export default function Comments({comments, recipeId}) {
+export default function Comments({comments, commentId}) {
 
-  const commentSection = comments.map(({ commentId, userName, message }) => (
-    <div key={commentId}>
-    <p>{userName}: {message}</p>
-  </div>
-  ));
+  const userName1 = useSelector((state) => state.userName)
+  const navigate = useNavigate()
+
+
+  const deleteComment = async (commentId, recipeId) => {
+    const info = {commentId: commentId}
+
+    const {data} = await axios.post('/api/deleteComment', info)
+    
+    navigate(`/recipes/${recipeId}`)
+  }
+
+  const commentSection = comments.map(({ commentId, userName, message, recipeId }) => {
+
+    if (userName === userName1) {
+      return (
+        <div key={commentId}>
+          <p>{userName}: {message}</p>
+          <div>
+            <button onClick={() => deleteComment(commentId, recipeId)}>Delete</button>
+          </div>
+        </div>
+      )
+    }
+    else {
+      return (
+        <div key={commentId}>
+          <p>{userName}: {message}</p>
+        </div>
+      )
+    }
+
+
+});
+
 
   return (
     <>
-      <AddComment/> 
-      {commentSection}
+    { userName1 &&
+      <>
+        <AddComment/> 
+        {commentSection}
+      </>
+    }
     </>
   )
 };
